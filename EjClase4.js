9a3d8547f092b4d687f09372f9f55a5c8ffcf2e0
Guadalupe <grripa@itba.edu.ar>
2025-09-01 14:05:04 -0300

    cambios

---
+++ EjClase4.js
@@ -0,0 +1,46 @@
+// 1. Ejercicio 1: Ciclo For
+// https://www.w3schools.com/js/js_loop_for.asp
+// Escribe un ciclo for que imprima los números pares entre 1 y 20.
+
+let 
+
+
+
+// 2. Ejercicio 
+// 2: Iterar un Array
+// https://www.w3schools.com/js/js_arrays.asp
+// Crea un array con los nombres de tus cinco películas favoritas y usa un ciclo for para 
+// imprimirlas con su respectivo número de posición (ej. "1. El Padrino").
+
+
+// 3. Ejercicio 3: Métodos de Arrays
+// https://www.w3schools.com/js/js_array_methods.asp
+// Usa el método forEach para recorrer un array de números y para cada número imprime si es par o impar.
+// Utiliza el método find para buscar el primer número mayor que 20 en un array de números.
+// Usa el método some para verificar si en un array de edades hay al menos una persona mayor de 18 años.
+
+
+// 4. Ejercicio 
+// 4: Iterar un Array con for...of
+// https://www.w3schools.com/js/js_loop_forof.asp
+// Crea un array con los nombres de 5 países y usa un ciclo for...of para imprimir cada uno.
+
+
+// 5. Ejercicio 5: Uso de forEach
+// https://www.w3schools.com/js/js_array_forEach.asp
+// Utiliza el método forEach para imprimir cada elemento de un array de números.
+
+
+// 6. Ejercicio 6: Métodos de Arrays Avanzados
+// https://www.w3schools.com/js/js_array_find.asp
+// Usa el método find para encontrar el primer número par en un array de números.
+// https://www.w3schools.com/js/js_array_some.asp
+// Emplea el método some para verificar si al menos un elemento en un array es mayor que 20.
+// https://www.w3schools.com/js/js_array_every.asp
+// Utiliza el método every para comprobar si todos los elementos en un array son strings.
+
+
+// 7. Ejercicio 7: Transformación de Arrays
+// https://www.w3schools.com/js/js_array_map.asp
+// Crea un array de objetos que representen personas con propiedades como nombre y edad. 
+// Usa map para crear un nuevo array que contenga solo los nombres.
